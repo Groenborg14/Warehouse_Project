d60c549754956d6da5f49af574794eaa99225ad3
groenborg14 <christian1409.g.m@gmail.com>
2026-09-20 21:22:52 +0200

    Added initial files, no code

---
+++ Src/WarehouseProject/CostSetter.py
@@ -0,0 +1,17 @@
+
+
+
+def check_startpoint():
+
+    # Chech if user has set a custom main road through warehouse and return P_l and P_r if so, else return False
+
+    return True
+
+
+def set_locals(P_l, P_r, input_data):
+
+    
+
+    return True
+
+
